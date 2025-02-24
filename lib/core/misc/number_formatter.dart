4d486870035b6ae4ffdfa11994742971c81dcebb
Anton <ton-An@outlook.de>
2025-02-24 18:53:45 +0100

    feat: Add number and time/date formatter classes for distance and duration

---
+++ lib/core/misc/number_formatter.dart
@@ -0,0 +1,8 @@
+import 'package:intl/intl.dart';
+
+class NumberFormatter {
+  static String formatDistance(double distance, String languageCode) {
+    final NumberFormat formatter = NumberFormat("0.#", languageCode);
+    return "${formatter.format(distance)} km";
+  }
+}
---
+++ lib/core/misc/time_date_formatter.dart
@@ -0,0 +1,16 @@
+import 'package:intl/intl.dart';
+
+class TimeDateFormatter {
+  static String getDuration(DateTime startTime, DateTime endTime) {
+    final Duration duration = endTime.difference(startTime);
+    if (duration.inHours > 0) {
+      return '${duration.inHours}h';
+    }
+    return '${duration.inMinutes}m';
+  }
+
+  static String getTimeFrame(
+      DateTime startTime, DateTime endTime, String languageCode) {
+    return "${DateFormat.Hm(languageCode).format(startTime)} - ${DateFormat.Hm(languageCode).format(endTime)}";
+  }
+}
