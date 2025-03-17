0eee553c9e72ae838abdf78d257a972ef030cf51
Anton <anton@antons-webfabrik.eu>
2025-03-17 14:24:32 +0100

    feat: Add RocketSimConnect loading functionality in AppDelegate

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,6 +8,17 @@ import UIKit
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    
+    loadRocketSimConnect()
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+    
+  // TODO: remove this from release versions. #DEBUG flag doesn't work because of flutter
+  private func loadRocketSimConnect() {
+      guard (Bundle(path: "/Applications/RocketSim.app/Contents/Frameworks/RocketSimConnectLinker.nocache.framework")?.load() == true) else {
+          print("Failed to load linker framework")
+          return
+      }
+      print("RocketSim Connect successfully linked")
+    }
 }
