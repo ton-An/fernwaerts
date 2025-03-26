4f095cf78d4e5751813bd531386471888cc136af
Anton <anton@antons-webfabrik.eu>
2025-03-26 14:30:21 +0100

    feat: implement create_initial_admin function for user setup and role assignment

---
+++ supabase/functions/create_initial_admin/index.ts
@@ -0,0 +1,101 @@
+import "jsr:@supabase/functions-js/edge-runtime.d.ts";
+import { createClient, SupabaseClient } from "jsr:@supabase/supabase-js";
+
+Deno.serve(async (req) => {
+  if (req.method !== "POST") {
+    return new Response(
+      JSON.stringify({ error: "Method not allowed" }),
+      {
+        status: 405,
+        headers: { "Content-Type": "application/json" },
+      },
+    );
+  }
+
+  const { username, email, password } = await req.json();
+
+  const supabaseClient = Deno.env.get("SUPABASE_CLIENT") ??
+    Deno.env.get("SUPABASE_URL");
+  const supabaseKey = Deno.env.get("SUPABASE_SERVICE_ROLE_KEY");
+
+  if (!supabaseClient || !supabaseKey) {
+    throw new Error("Missing Supabase environment variables");
+  }
+
+  const supabase = createClient(supabaseClient, supabaseKey);
+
+  if (await isSetUp(supabase)) {
+    return new Response(
+      JSON.stringify({ error: "Server already set up" }),
+      {
+        status: 400,
+        headers: { "Content-Type": "application/json" },
+      },
+    );
+  }
+
+  const userId = await createUserAccount(supabase, email, password);
+
+  await addUserToDB(supabase, userId, username, email);
+
+  await makeUserAdmin(supabase, userId);
+
+  await markSetupAsComplete(supabase);
+
+  return new Response();
+});
+
+async function createUserAccount(
+  supabase: SupabaseClient,
+  email: string,
+  password: string,
+): Promise<string> {
+  const { data } = await supabase.auth.admin.createUser({
+    email,
+    password,
+  });
+
+  return data.user?.id;
+}
+
+async function isSetUp(supabase: SupabaseClient): Promise<boolean> {
+  const { data } = await supabase.from("public_settings")
+    .select().eq(
+      "name",
+      "is_set_up",
+    ).single();
+
+  return data.value;
+}
+
+async function addUserToDB(
+  supabase: SupabaseClient,
+  userId: string,
+  username: string,
+  email: string,
+) {
+  await supabase.from("users").insert({
+    id: userId,
+    username: username,
+    email: email,
+  });
+}
+
+async function makeUserAdmin(
+  supabase: SupabaseClient,
+  userId: string,
+) {
+  await supabase.from("user_roles").insert({
+    user_id: userId,
+    role: "admin",
+  });
+}
+
+async function markSetupAsComplete(supabase: SupabaseClient) {
+  await supabase.from("public_settings").update({
+    value: true,
+  }).eq(
+    "name",
+    "is_set_up",
+  );
+}
