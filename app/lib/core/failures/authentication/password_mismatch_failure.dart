4386f4822e5bca51c6d4d7098ddae48ad55edfe2
Anton <anton@antons-webfabrik.eu>
2025-03-28 16:26:40 +0100

    [app] feat(failures): add PasswordMismatchFailure class

---
+++ app/lib/core/failures/authentication/password_mismatch_failure.dart
@@ -0,0 +1,12 @@
+import 'package:location_history/core/failures/failure.dart';
+import 'package:location_history/core/failures/failure_constants.dart';
+
+class PasswordMismatchFailure extends Failure {
+  const PasswordMismatchFailure()
+    : super(
+        name: "Password Mismatch",
+        message: "The provided passwords do not match.",
+        categoryCode: FailureCategoryConstants.authentication,
+        code: "password_mismatch",
+      );
+}
