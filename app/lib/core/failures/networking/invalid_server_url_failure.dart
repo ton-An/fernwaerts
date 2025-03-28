eb418101feb976f14df00596d17fb8dd61d9f4cb
Anton <anton@antons-webfabrik.eu>
2025-03-28 14:57:00 +0100

    [app] feat(failures): add InvalidUrlFormatFailure class for handling invalid server URL formats

---
+++ app/lib/core/failures/networking/invalid_server_url_failure.dart
@@ -0,0 +1,12 @@
+import 'package:location_history/core/failures/failure.dart';
+import 'package:location_history/core/failures/failure_constants.dart';
+
+class InvalidUrlFormatFailure extends Failure {
+  const InvalidUrlFormatFailure()
+    : super(
+        name: "Invalid Server Url Format",
+        message: "The server URL you entered is formatted incorrectly.",
+        categoryCode: FailureCategoryConstants.networking,
+        code: "invalid_server_url_format",
+      );
+}
