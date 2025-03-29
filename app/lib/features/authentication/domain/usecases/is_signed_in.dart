039a8e0a655acecba223f9337d762c0708d11e93
Anton <anton@antons-webfabrik.eu>
2025-03-29 08:48:24 +0100

    [app] feat(auth): add IsSignedIn use case to check user authentication status

---
+++ app/lib/features/authentication/domain/usecases/is_signed_in.dart
@@ -0,0 +1,29 @@
+import 'package:dartz/dartz.dart';
+import 'package:location_history/core/failures/failure.dart';
+import 'package:location_history/features/authentication/domain/repositories/authentication_repository.dart';
+
+/*
+  To-Do:
+    - [ ] Add Failures to docs
+*/
+
+/// {@template is_signed_in}
+/// Checks if the user is signed in
+///
+/// Returns:
+/// - a [bool] indicating if the user is signed in
+///
+/// Failures:
+/// - TBD
+/// {@endtemplate}
+class IsSignedIn {
+  /// {@macro is_signed_in}
+  const IsSignedIn({required this.authenticationRepository});
+
+  final AuthenticationRepository authenticationRepository;
+
+  /// {@macro is_signed_in}
+  Future<Either<Failure, bool>> call() async {
+    return await authenticationRepository.isSignedIn();
+  }
+}
