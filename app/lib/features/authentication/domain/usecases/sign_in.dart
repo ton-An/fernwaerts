7f77ff16d421d65328544d90ecc5e89c8f9c3259
Anton <anton@antons-webfabrik.eu>
2025-03-29 09:17:26 +0100

    [app] feat(auth): implement SignIn use case for user authentication

---
+++ app/lib/features/authentication/domain/usecases/sign_in.dart
@@ -0,0 +1,36 @@
+import 'package:dartz/dartz.dart';
+import 'package:location_history/core/failures/failure.dart';
+import 'package:location_history/features/authentication/domain/repositories/authentication_repository.dart';
+
+/*
+  To-Do:
+    - [ ] Add Failures to docs
+*/
+
+/// {@template sign_in}
+/// Signs in a user with a given username and password
+///
+/// Parameters:
+/// - [String] username
+/// - [String] password
+///
+/// Failures:
+/// - TBD
+/// {@endtemplate}
+class SignIn {
+  /// {@macro sign_in}
+  const SignIn({required this.authenticationRepository});
+
+  final AuthenticationRepository authenticationRepository;
+
+  /// {@macro sign_in}
+  Future<Either<Failure, None>> call({
+    required String username,
+    required String password,
+  }) {
+    return authenticationRepository.signIn(
+      username: username,
+      password: password,
+    );
+  }
+}
