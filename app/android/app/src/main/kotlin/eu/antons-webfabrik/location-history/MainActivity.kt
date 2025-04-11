d50cfe700d048b225778ade9e4968f73906b3b43
Anton <anton@antons-webfabrik.eu>
2025-04-11 21:36:05 +0200

    [app] fix: update namespace and application ID format to adhere to ios package naming standards

--- app/android/app/src/main/kotlin/eu/antons-webfabrik/location-history/MainActivity.kt
+++ app/android/app/src/main/kotlin/eu/antons-webfabrik/location-history/MainActivity.kt
@@ -1,4 +1,4 @@
-package eu.antons_webfabrik.location_history-an.location_history
+package eu.antons-webfabrik.location-history-an.location_history
 
 import io.flutter.embedding.android.FlutterActivity
 
