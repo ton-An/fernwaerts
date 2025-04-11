2ac4562650321ff11102562e1effc136e3f387e3
Anton <anton@antons-webfabrik.eu>
2025-04-11 18:32:30 +0200

    [app] feat(map): add user agent package name to map tile server request

--- app/lib/features/map/presentation/widgets/map_widget.dart
+++ app/lib/features/map/presentation/widgets/map_widget.dart
@@ -1,9 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
-class MapWidget extends StatelessWidget {
+class MapWidget extends StatefulWidget {
   const MapWidget({super.key});
+
+  @override
+  State<MapWidget> createState() => _MapWidgetState();
+}
+
+class _MapWidgetState extends State<MapWidget> {
+  String? appPackageName;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _setAppPackageName();
+  }
+
+  Future<void> _setAppPackageName() async {
+    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+
+    setState(() {
+      appPackageName = packageInfo.packageName;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +38,7 @@ class MapWidget extends StatelessWidget {
       children: [
         TileLayer(
           urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-          userAgentPackageName: 'com.example.app',
+          userAgentPackageName: appPackageName ?? 'unknown',
         ),
       ],
     );
