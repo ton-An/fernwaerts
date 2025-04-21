30505e3fda5e55c24b214aa79d627620612eaf7c
Anton <anton@antons-webfabrik.eu>
2025-04-21 17:05:44 +0200

    [supabase] feat(functions): add function to serve Supabase anon key

---
+++ supabase/functions/get_anon_key/index.ts
@@ -0,0 +1,23 @@
+import "jsr:@supabase/functions-js/edge-runtime.d.ts";
+
+Deno.serve(async (req) => {
+  if (req.method !== "GET") {
+    return new Response(
+      JSON.stringify({ error: "Method not allowed" }),
+      {
+        status: 405,
+        headers: { "Content-Type": "application/json" },
+      },
+    );
+  }
+
+  const anonKey = Deno.env.get("SUPABASE_ANON_KEY");
+
+  if (!anonKey) {
+    throw new Error("Missing Supabase environment variables");
+  }
+
+  return new Response(JSON.stringify({ "data": { "anonKey": anonKey } }), {
+    status: 200,
+  });
+});
