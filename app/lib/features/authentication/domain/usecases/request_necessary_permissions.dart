98b588ad1b35260739316ccd2f6fea5e311ca5df
Anton <anton@antons-webfabrik.eu>
2025-04-26 23:02:06 +0200

    [app] docs(auth): add docs to request necessary permissions usecase

--- app/lib/features/authentication/domain/usecases/request_necessary_permissions.dart
+++ app/lib/features/authentication/domain/usecases/request_necessary_permissions.dart
@@ -1,12 +1,23 @@
 import 'package:fpdart/fpdart.dart';
 import 'package:location_history/core/failures/failure.dart';
+import 'package:location_history/core/failures/permission/activity_permission_not_granted_failure.dart';
+import 'package:location_history/core/failures/permission/location_permission_not_granted_failure.dart';
 import 'package:location_history/features/authentication/domain/repositories/permissions_repository.dart';
 
+/// {@template request_necessary_permissions}
+/// Requests necessary permissions for the application.
+///
+/// Failures:
+/// - [LocationPermissionNotGrantedFailure]
+/// - [ActivityPermissionNotGrantedFailure]
+/// {@endtemplate}
 class RequestNecessaryPermissions {
+  /// {@macro request_necessary_permissions}
   const RequestNecessaryPermissions({required this.permissionsRepository});
 
   final PermissionsRepository permissionsRepository;
 
+  /// {@macro request_necessary_permissions}
   Future<Either<Failure, None>> call() async {
     return _requestLocationPermission();
   }
