5de755dc41a65d20017b2bf0dd1be98764ba572b
Anton <anton@antons-webfabrik.eu>
2025-04-28 12:32:20 +0200

    [app] feat: add use case files for location tracking with comment on what they will do

---
+++ app/lib/features/location_tracking/domain/usecases/clean_up_recorded_data.dart
@@ -0,0 +1,6 @@
+// Cleans up the recorded data (needed when app comes from background). 
+// Includes: 
+// - Querying historical activity data from os
+// - Creating activities in the database
+//     - This includes an algorithm deciding the start and end points
+// - ... probably more
---
+++ app/lib/features/location_tracking/domain/usecases/significant_location_change_stream.dart
@@ -0,0 +1,4 @@
+// Stream of significant location changes
+// Includes:
+// - Starting/stopping fine grained tracking
+//     - Use activity data to save battery
