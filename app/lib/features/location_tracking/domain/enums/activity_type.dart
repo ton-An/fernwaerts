ae38aada1ec2483fa2e1a48faddb4e938c619c67
Anton <anton@antons-webfabrik.eu>
2025-05-01 18:35:08 +0200

    [app] feat(tracking): impl model conversion from bg activity to fernwaerts activity

--- app/lib/features/location_tracking/domain/enums/activity_type.dart
+++ app/lib/features/location_tracking/domain/enums/activity_type.dart
@@ -3,4 +3,31 @@
     - [ ] Add all possible activity types
 */
 
-enum ActivityType { walking, still, unknown }
+enum ActivityType {
+  still,
+  walking,
+  onFoot,
+  running,
+  onBicycle,
+  inVehicle,
+  unknown;
+
+  static ActivityType fromBGActivityType({required String bgActivityType}) {
+    switch (bgActivityType) {
+      case 'still':
+        return ActivityType.still;
+      case 'walking':
+        return ActivityType.walking;
+      case 'on_foot':
+        return ActivityType.onFoot;
+      case 'running':
+        return ActivityType.running;
+      case 'on_bicycle':
+        return ActivityType.onBicycle;
+      case 'in_vehicle':
+        return ActivityType.inVehicle;
+      default:
+        return ActivityType.unknown;
+    }
+  }
+}
--- app/lib/features/location_tracking/domain/models/activity.dart
+++ app/lib/features/location_tracking/domain/models/activity.dart
@@ -1,8 +1,17 @@
-import 'package:flutter_activity_recognition/models/activity_type.dart';
+import 'package:flutter_background_geolocation/flutter_background_geolocation.dart'
+    as bg;
+import 'package:location_history/features/location_tracking/domain/enums/activity_type.dart';
 
 class Activity {
   Activity({required this.type, required this.confidence});
 
   final ActivityType type;
   final double confidence;
+
+  static Activity fromBGActivity(bg.Activity bgActivity) {
+    return Activity(
+      type: ActivityType.fromBGActivityType(bgActivityType: bgActivity.type),
+      confidence: bgActivity.confidence / 100,
+    );
+  }
 }
