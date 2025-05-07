5cfcd4d7c9f1945daab5063aa9c59d23a6115ff0
Anton <anton@antons-webfabrik.eu>
2025-05-07 14:40:48 +0200

    [app] feat(tracking): implement get locations by date usecase

---
+++ app/lib/features/location_tracking/domain/usecases/get_locations_by_date.dart
@@ -0,0 +1,42 @@
+import 'package:fpdart/fpdart.dart';
+import 'package:location_history/core/failures/failure.dart';
+import 'package:location_history/features/authentication/domain/repositories/authentication_repository.dart';
+import 'package:location_history/features/location_tracking/domain/models/location.model.dart';
+import 'package:location_history/features/location_tracking/domain/repositories/location_data_repository.dart';
+
+class GetLocationsByDate {
+  const GetLocationsByDate({
+    required this.authenticationRepository,
+    required this.locationDataRepository,
+  });
+
+  final AuthenticationRepository authenticationRepository;
+  final LocationDataRepository locationDataRepository;
+
+  Future<Either<Failure, List<Location>>> call({
+    required DateTime start,
+    required DateTime end,
+  }) async {
+    return _getCurrentUserId(start: start, end: end);
+  }
+
+  Future<Either<Failure, List<Location>>> _getCurrentUserId({
+    required DateTime start,
+    required DateTime end,
+  }) async {
+    final Either<Failure, String> userIdEither =
+        await authenticationRepository.getCurrentUserId();
+
+    return userIdEither.fold(Left.new, (String userId) async {
+      return _getLocationsByDate(start: start, end: end, userId: userId);
+    });
+  }
+
+  Future<Either<Failure, List<Location>>> _getLocationsByDate({
+    required DateTime start,
+    required DateTime end,
+    required String userId,
+  }) async {
+    return locationDataRepository.getLocationsByDate(start: start, end: end);
+  }
+}
