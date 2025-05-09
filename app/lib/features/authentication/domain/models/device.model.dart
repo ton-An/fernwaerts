c808f1b1418547606b80040e16f9da47cdc27cac
Anton <anton@antons-webfabrik.eu>
2025-05-09 15:55:08 +0200

    [app] style: add ignore directive for must_be_immutable triggered by id field to device model

--- app/lib/features/authentication/domain/models/device.model.dart
+++ app/lib/features/authentication/domain/models/device.model.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: must_be_immutable
+
 import 'package:brick_offline_first_with_supabase/brick_offline_first_with_supabase.dart';
 import 'package:brick_supabase/brick_supabase.dart';
 import 'package:equatable/equatable.dart';
