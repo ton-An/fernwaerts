6900b5cfc59e472eb53c99d9fe5aa0bac9c4d913
Anton <anton@antons-webfabrik.eu>
2025-05-13 14:34:45 +0200

    [app] feat(core): add jwt refresh client for brick supabase calls

--- app/lib/core/data/datasources/supabase_offline_first.dart
+++ app/lib/core/data/datasources/supabase_offline_first.dart
@@ -6,6 +6,7 @@ import 'package:brick_sqlite/memory_cache_provider.dart';
 import 'package:brick_supabase/brick_supabase.dart' hide Supabase;
 import 'package:location_history/brick/brick.g.dart';
 import 'package:location_history/brick/db/schema.g.dart';
+import 'package:location_history/core/data/datasources/jwt_refresh_client.dart';
 import 'package:sqflite/sqflite.dart' show databaseFactory;
 import 'package:supabase_flutter/supabase_flutter.dart';
 
@@ -29,6 +30,8 @@ class SupabaseOfflineFirst
   }) async {
     final (client, queue) = OfflineFirstWithSupabaseRepository.clientQueue(
       databaseFactory: databaseFactory,
+      innerClient: JWTRefreshClient(),
+      processingInterval: Duration.zero,
     );
 
     final supabase = await Supabase.initialize(
@@ -42,13 +45,15 @@ class SupabaseOfflineFirst
       modelDictionary: supabaseModelDictionary,
     );
 
+    final SqliteProvider sqliteProvider = SqliteProvider(
+      'my_repository.sqlite',
+      databaseFactory: databaseFactory,
+      modelDictionary: sqliteModelDictionary,
+    );
+
     _singleton = SupabaseOfflineFirst._(
       supabaseProvider: provider,
-      sqliteProvider: SqliteProvider(
-        'my_repository.sqlite',
-        databaseFactory: databaseFactory,
-        modelDictionary: sqliteModelDictionary,
-      ),
+      sqliteProvider: sqliteProvider,
       migrations: migrations,
       offlineRequestQueue: queue,
       memoryCacheProvider: MemoryCacheProvider(),
