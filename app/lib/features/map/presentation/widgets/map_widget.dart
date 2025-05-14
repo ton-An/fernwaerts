4f08e2c7aeaed69f7e3a12c6be13b9a9b62e57b2
Anton <anton@antons-webfabrik.eu>
2025-05-14 12:14:15 +0200

    [app] feat(map): add color gradient to points on map signaling passing of time

--- app/lib/features/map/presentation/widgets/map_widget.dart
+++ app/lib/features/map/presentation/widgets/map_widget.dart
@@ -5,11 +5,16 @@ import 'package:latlong2/latlong.dart';
 import 'package:location_history/core/misc/date_time_extensions.dart';
 import 'package:location_history/features/calendar/presentation/cubits/calendar_date_selection_cubit/calendar_date_selection_cubit.dart';
 import 'package:location_history/features/calendar/presentation/cubits/calendar_date_selection_cubit/calendar_date_selection_state.dart';
+import 'package:location_history/features/in_app_notification/presentation/cubit/in_app_notification_cubit.dart';
 import 'package:location_history/features/map/presentation/cubits/map_cubit.dart';
 import 'package:location_history/features/map/presentation/cubits/map_states.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:webfabrik_theme/webfabrik_theme.dart';
 
+/* 
+  To-Do:
+    - [ ] Improve implementation of gradient (include theme etc..)
+*/
 class MapWidget extends StatefulWidget {
   const MapWidget({super.key});
 
@@ -62,7 +67,7 @@ class _MapWidgetState extends State<MapWidget> {
     });
   }
 
-  final List<LatLng> _points = [];
+  final List<LatLng> _points = [LatLng(0, 0)];
 
   @override
   Widget build(BuildContext context) {
@@ -76,6 +81,7 @@ class _MapWidgetState extends State<MapWidget> {
         listener: (context, mapState) {
           if (mapState is MapLocationsLoaded) {
             final points = <LatLng>[];
+
             for (final location in mapState.locations) {
               points.add(
                 LatLng(
@@ -90,6 +96,12 @@ class _MapWidgetState extends State<MapWidget> {
               _points.addAll(points);
             });
           }
+
+          if (mapState is MapLocationsError) {
+            context.read<InAppNotificationCubit>().sendFailureNotification(
+              mapState.failure,
+            );
+          }
         },
         child: FlutterMap(
           options: const MapOptions(
@@ -103,13 +115,29 @@ class _MapWidgetState extends State<MapWidget> {
             ),
             CircleLayer(
               circles: [
-                for (final LatLng point in _points)
+                for (int i = 0; i < _points.length; i++)
                   CircleMarker(
-                    point: point,
+                    point: _points[i],
                     radius: 10,
-                    color: theme.colors.primary,
-                    borderStrokeWidth: 10,
-                    borderColor: theme.colors.primaryTranslucent,
+                    color: interpolateColors([
+                      Colors.purple,
+                      Colors.blue,
+                      Colors.teal,
+                      Colors.green,
+                      Colors.yellow,
+                      Colors.orange,
+                      Colors.red,
+                    ], i / _points.length),
+                    borderStrokeWidth: 8,
+                    borderColor: interpolateColors([
+                      Colors.purple,
+                      Colors.blue,
+                      Colors.teal,
+                      Colors.green,
+                      Colors.yellow,
+                      Colors.orange,
+                      Colors.red,
+                    ], i / _points.length).withValues(alpha: .3),
                   ),
               ],
             ),
@@ -117,5 +145,22 @@ class _MapWidgetState extends State<MapWidget> {
         ),
       ),
     );
+  }
+
+  Color interpolateColors(List<Color> colors, double t) {
+    if (colors.isEmpty) throw ArgumentError("Colors list cannot be empty");
+    if (colors.length == 1 || t <= 0.0) return colors.first;
+    if (t >= 1.0) return colors.last;
+
+    final segmentLength = 1.0 / (colors.length - 1);
+    final segmentIndex = (t / segmentLength).floor();
+    final localT = (t - (segmentIndex * segmentLength)) / segmentLength;
+
+    final colorStart = colors[segmentIndex];
+    final colorEnd = colors[segmentIndex + 1];
+
+    Color color = Color.lerp(colorStart, colorEnd, localT)!;
+
+    return color;
   }
 }
