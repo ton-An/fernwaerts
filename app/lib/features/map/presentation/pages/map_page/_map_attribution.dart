0fe9d534a86d51ccd75554088b9f7368b6481421
Anton <anton@antons-webfabrik.eu>
2025-05-14 12:14:55 +0200

    [app] feat(map): add time passing gradient legend

--- app/lib/features/map/presentation/pages/map_page/_map_attribution.dart
+++ app/lib/features/map/presentation/pages/map_page/_map_attribution.dart
@@ -23,23 +23,18 @@ class _MapAttribution extends StatelessWidget {
                     : 0
                 : 1,
         curve: Curves.easeOut,
-        child: Padding(
-          padding: EdgeInsets.only(
-            right: WebfabrikTheme.of(context).spacing.medium,
-          ),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(theme.radii.small),
-            child: BackdropFilter(
-              filter: theme.misc.blurFilter,
-              child: Container(
-                padding: EdgeInsets.all(theme.spacing.xxSmall),
-                color: theme.colors.translucentBackground,
-                child: Text(
-                  AppLocalizations.of(context)!.openStreetMapAttribution,
-                  style: theme.text.subhead.copyWith(
-                    height: 1,
-                    color: theme.colors.text.withValues(alpha: .7),
-                  ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(theme.radii.small),
+          child: BackdropFilter(
+            filter: theme.misc.blurFilter,
+            child: Container(
+              padding: EdgeInsets.all(theme.spacing.xxSmall),
+              color: theme.colors.translucentBackground,
+              child: Text(
+                AppLocalizations.of(context)!.openStreetMapAttribution,
+                style: theme.text.subhead.copyWith(
+                  height: 1,
+                  color: theme.colors.text.withValues(alpha: .7),
                 ),
               ),
             ),
