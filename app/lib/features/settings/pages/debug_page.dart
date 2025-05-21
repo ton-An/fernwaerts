cb1722e66387a2d7cf27053cc7f71cd69ce4d76f
Anton <anton@antons-webfabrik.eu>
2025-05-21 14:17:01 +0200

    [app] feat(settings): add debug page

---
+++ app/lib/features/settings/pages/debug_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/widgets.dart';
+import 'package:location_history/core/dependency_injector.dart';
+import 'package:location_history/core/l10n/app_localizations.dart';
+import 'package:talker_flutter/talker_flutter.dart';
+import 'package:webfabrik_theme/webfabrik_theme.dart';
+
+class DebugPage extends StatelessWidget {
+  const DebugPage({super.key});
+
+  static const String pageName = 'debug';
+  static const String route = '/$pageName';
+
+  @override
+  Widget build(BuildContext context) {
+    final WebfabrikThemeData theme = WebfabrikTheme.of(context);
+
+    final Talker talker = getIt<Talker>();
+
+    return TalkerScreen(
+      talker: talker,
+      appBarTitle: AppLocalizations.of(context)!.debugger,
+      theme: TalkerScreenTheme(
+        backgroundColor: theme.colors.background,
+        textColor: theme.colors.text,
+        cardColor: theme.colors.fieldColor,
+      ),
+    );
+  }
+}
