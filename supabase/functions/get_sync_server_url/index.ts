a2473806a0219ce561de166b01028402c41f9818
Anton <anton@antons-webfabrik.eu>
2025-08-07 15:12:54 +0200

    [supabase] feat: add auth to get powersync url

--- supabase/functions/get_sync_server_url/index.ts
+++ supabase/functions/get_sync_server_url/index.ts
@@ -1,6 +1,23 @@
 import "jsr:@supabase/functions-js/edge-runtime.d.ts";
+import { createClient } from "npm:@supabase/supabase-js@2";
 
-Deno.serve(() => {
+Deno.serve(async (request) => {
+  const supabase = createClient(
+    Deno.env.get("SUPABASE_URL") ?? "",
+    Deno.env.get("SUPABASE_SERVICE_ROLE_KEY") ?? "",
+  );
+
+  const token = request.headers.get("Authorization")?.replace("Bearer ", "");
+
+  const {
+    data: { user },
+    error,
+  } = await supabase.auth.getUser(token);
+
+  if (error || !user) {
+    return new Response("Unauthorized", { status: 401 });
+  }
+
   const syncServerUrl = Deno.env.get("POWERSYNC_URL");
 
   if (!syncServerUrl) {
