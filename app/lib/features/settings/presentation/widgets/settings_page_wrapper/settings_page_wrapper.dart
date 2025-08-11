00496c084dddfa005da0140a9d0749b9501f0e22
Anton <anton@antons-webfabrik.eu>
2025-08-11 20:15:54 +0200

    [app] feat(settings): change page title for user management settings

--- app/lib/features/settings/presentation/widgets/settings_page_wrapper/settings_page_wrapper.dart
+++ app/lib/features/settings/presentation/widgets/settings_page_wrapper/settings_page_wrapper.dart
@@ -63,7 +63,7 @@ class SettingsPageWrapper extends StatelessWidget {
     if (pagePath == AccountSettingsPage.route) {
       return AppLocalizations.of(context)!.account;
     } else if (pagePath == UserManagementSettingsPage.route) {
-      return AppLocalizations.of(context)!.userManagement;
+      return AppLocalizations.of(context)!.users;
     } else if (pagePath == MainSettingsPage.route) {}
 
     return AppLocalizations.of(context)!.settings;
