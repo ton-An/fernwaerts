c3637072ab396f1c4401adf6f397d368c2fc1814
Anton <anton@antons-webfabrik.eu>
2026-05-25 16:02:38 +0200

    [supabase] refactor(schema/migrations): remove timestamp fields from role_permissions table

--- supabase/schemas/role_permissions/20_table.sql
+++ supabase/schemas/role_permissions/20_table.sql
@@ -2,8 +2,5 @@ create table "public"."role_permissions" (
   "id" uuid not null default gen_random_uuid(),
   "role" public.roles not null,
   "permission" public.permissions not null,
-  "created_at" timestamp with time zone not null default (now() AT TIME ZONE 'utc'::text),
-  "updated_at" timestamp with time zone not null default (now() AT TIME ZONE 'utc'::text),
-  "deleted_at" timestamp with time zone,
   primary key ("id")
     );
