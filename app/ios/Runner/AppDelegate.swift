f6575e6410ecb4ff617ad94a10cd25c5db449231
Anton <anton@antons-webfabrik.eu>
2026-06-06 16:20:04 +0200

    [app] chore(ios): UISceneDelegate migration

--- app/ios/Runner/AppDelegate.swift
+++ app/ios/Runner/AppDelegate.swift
@@ -2,28 +2,15 @@ import Flutter
 import UIKit
 
 @main
-@objc class AppDelegate: FlutterAppDelegate {
+@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
     override func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
-        GeneratedPluginRegistrant.register(with: self)
-
-        loadRocketSimConnect()
         return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
 
-    // TODO: remove this from release versions. #DEBUG flag doesn't work because of flutter
-    private func loadRocketSimConnect() {
-        guard
-            Bundle(
-                path:
-                    "/Applications/RocketSim.app/Contents/Frameworks/RocketSimConnectLinker.nocache.framework"
-            )?.load() == true
-        else {
-            print("Failed to load linker framework")
-            return
-        }
-        print("RocketSim Connect successfully linked")
+    func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
+        GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
     }
 }
